d28e5ce8bfcdbce4925e62a7ca9eeb82a0b0a384
Jan Carlo Cardama <jcmcardama@gmail.com>
2026-06-19 04:45:03 +0800

    cleanup of unecessary files

--- src/app/AppStyling.tsx
+++
@@ -1,7 +0,0 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-
-const theme = createTheme({ palette: { mode: 'dark' } });
-
-export const AppStyling = ({ children }: { children: React.ReactNode }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-);
